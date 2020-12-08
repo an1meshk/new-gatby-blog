e8e3a54afd5351b7f426af96c23e6bf049fceba2
Animesh Kumar <animeshkumar@Animeshs-MacBook-Pro.local>
2020-12-07 18:29:43 -0600

    Update content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Yihua's Blog`,
+    title: `AKS's Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default ({ data }) => {
     <Layout>
       <SEO title="Home" />
       <div>
-        <h1>Yihua's Thoughts</h1>
+        <h1>AKS's Thoughts</h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
